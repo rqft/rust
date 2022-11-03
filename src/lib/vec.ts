bf215417935ca7f80319d13c9989bb543f2af9a8
mina <higharcswastaken@gmail.com>
2022-11-02 21:52:49 -0500

    whoops, fix eslint issues

--- src/lib/vec.ts
+++ src/lib/vec.ts
@@ -4,18 +4,17 @@ import type { FnMap, FnOnce } from './traits';
 
 export class Vec<T> {
   private readonly alloc: Array<Option<T>> = [];
-  constructor() {}
-  public static new<T>() {
+  public static new<T>(): Vec<T> {
     return new this<T>();
   }
 
-  *[Symbol.iterator]() {
+  *[Symbol.iterator](): Generator<Option<T>, void, unknown> {
     while (this.len() > 0) {
-      this.pop();
+      yield this.pop();
     }
   }
 
-  public static withCapacity<T>(capacity: number) {
+  public static withCapacity<T>(capacity: number): Vec<T> {
     const p = this.new<T>();
     let i = 0;
     while (i++ < capacity) {
@@ -25,11 +24,11 @@ export class Vec<T> {
     return p;
   }
 
-  public capacity() {
+  public capacity(): number {
     return this.alloc.length;
   }
 
-  public reserve(max: number) {
+  public reserve(max: number): this {
     while (this.capacity() < max) {
       this.alloc.push(None);
     }
@@ -37,7 +36,7 @@ export class Vec<T> {
     return this;
   }
 
-  public len() {
+  public len(): number {
     let c = 0;
     let i = 0;
     while (this.alloc[i++]?.isSome()) {
@@ -47,7 +46,7 @@ export class Vec<T> {
     return c;
   }
 
-  public shrinkToFit() {
+  public shrinkToFit(): this {
     while (this.hasTrail()) {
       delete this.alloc[this.capacity() - 1];
     }
@@ -55,7 +54,7 @@ export class Vec<T> {
     return this;
   }
 
-  public shrinkTo(minCapacity: number) {
+  public shrinkTo(minCapacity: number): this {
     while (this.capacity() > minCapacity && this.hasTrail()) {
       delete this.alloc[this.capacity() - 1];
     }
@@ -63,15 +62,15 @@ export class Vec<T> {
     return this;
   }
 
-  private hasTrail() {
+  private hasTrail(): boolean {
     return this.alloc[this.capacity() - 1]?.isNone() ?? true;
   }
 
-  private last() {
+  private last(): Option<T> {
     return this.get(this.len() - 1);
   }
 
-  public truncate(len: number) {
+  public truncate(len: number): this {
     while (this.capacity() < len) {
       delete this.alloc[this.capacity() - 1];
     }
@@ -83,7 +82,7 @@ export class Vec<T> {
     throw `oob: ${index} does not fit in vec[${this.len()}; ${this.capacity()}]`;
   }
 
-  public swapRemove(index: number) {
+  public swapRemove(index: number): Option<T> {
     const value = this.alloc[index];
     if (value === None || value === undefined) {
       // oob: -1 does not fit in vec[3; 5]
@@ -95,7 +94,7 @@ export class Vec<T> {
     return value;
   }
 
-  public insert(index: number, element: T) {
+  public insert(index: number, element: T): this {
     if (index > this.len()) {
       throw this.oob(index);
     }
@@ -104,7 +103,7 @@ export class Vec<T> {
     return this;
   }
 
-  public remove(index: number) {
+  public remove(index: number): Option<T> {
     if (index > this.len()) {
       throw this.oob(index);
     }
@@ -112,7 +111,7 @@ export class Vec<T> {
     return this.alloc.splice(index, 1)[0] || None;
   }
 
-  public retain(f: FnMap<T, boolean>) {
+  public retain(f: FnMap<T, boolean>): this {
     let i = 0;
     while (i < this.len()) {
       if (!f(this.get(i).unwrap())) {
@@ -125,7 +124,7 @@ export class Vec<T> {
     return this;
   }
 
-  public push(element: T) {
+  public push(element: T): this {
     if (this.hasTrail()) {
       this.alloc[this.alloc.indexOf(None)] = Some(element);
     } else {
@@ -135,18 +134,18 @@ export class Vec<T> {
     return this;
   }
 
-  public pop() {
+  public pop(): Option<T> {
     if (this.len() === 0) {
       return None;
     }
 
-    const i = this.alloc[this.len() - 1];
+    const i = this.get(this.len() - 1);
     this.remove(this.len() - 1);
 
     return i;
   }
 
-  public append(other: Vec<T>) {
+  public append(other: Vec<T>): this {
     while (other.len()) {
       this.push((other.pop() || None).unwrap());
     }
@@ -154,7 +153,7 @@ export class Vec<T> {
     return this;
   }
 
-  public drain([range_min, range_max]: [number, number]) {
+  public drain([range_min, range_max]: [number, number]): Vec<unknown> {
     if (range_min > range_max) {
       throw 'starting point cannot be larger than the ending point';
     }
@@ -172,7 +171,7 @@ export class Vec<T> {
     return n;
   }
 
-  public drainFilter(f: FnMap<T, boolean>) {
+  public drainFilter(f: FnMap<T, boolean>): void {
     let i = 0;
     const n = Vec.new();
     while (i < this.len()) {
@@ -185,19 +184,19 @@ export class Vec<T> {
     }
   }
 
-  public clear() {
-    while (this.last()) {
+  public clear(): this {
+    while (this.last().isSome()) {
       this.alloc[this.len() - 1] = None;
     }
 
     return this;
   }
 
-  public isEmpty() {
+  public isEmpty(): boolean {
     return this.len() === 0;
   }
 
-  public splitOff(at: number) {
+  public splitOff(at: number): Vec<unknown> {
     if (at > this.len()) {
       throw this.oob(at);
     }
@@ -212,7 +211,7 @@ export class Vec<T> {
     return p;
   }
 
-  public resizeWith(newLen: number, f: FnOnce<T>) {
+  public resizeWith(newLen: number, f: FnOnce<T>): this {
     if (newLen < this.len()) {
       return this.truncate(newLen);
     }
@@ -224,11 +223,11 @@ export class Vec<T> {
     return this;
   }
 
-  public resize(newLen: number, t: T) {
+  public resize(newLen: number, t: T): this {
     return this.resizeWith(newLen, () => t);
   }
 
-  public extend(iter: Iterable<T>) {
+  public extend(iter: Iterable<T>): this {
     for (const value of iter) {
       this.push(value);
     }
@@ -236,14 +235,14 @@ export class Vec<T> {
     return this;
   }
 
-  public join(str: string) {
+  public join(str: string): string {
     return this.alloc
       .filter((x) => x.isSome())
       .map((x) => x.unwrap())
       .join(str);
   }
 
-  public contains(value: T) {
+  public contains(value: T): boolean {
     for (let i = 0; i < this.len(); i++) {
       if (this.get(i).unwrap() === value) {
         return true;
@@ -253,7 +252,7 @@ export class Vec<T> {
     return false;
   }
 
-  public endsWith(...needle: Array<T>) {
+  public endsWith(...needle: Array<T>): boolean {
     if (this.len() === 0 || needle.length === 0) {
       return true;
     }
@@ -268,7 +267,7 @@ export class Vec<T> {
     return true;
   }
 
-  public fill(value: T) {
+  public fill(value: T): this {
     for (let i = 0; i < this.capacity(); i++) {
       this.alloc[i] = Some(value);
     }
@@ -276,7 +275,7 @@ export class Vec<T> {
     return this;
   }
 
-  public fillWith(f: FnOnce<T>) {
+  public fillWith(f: FnOnce<T>): this {
     for (let i = 0; i < this.capacity(); i++) {
       this.alloc[i] = Some(f());
     }
@@ -284,26 +283,26 @@ export class Vec<T> {
     return this;
   }
 
-  public first() {
+  public first(): Option<T> {
     return this.get(0);
   }
 
-  public get(index: number) {
+  public get(index: number): Option<T> {
     return this.alloc[index] || None;
   }
 
-  public static repeat<T>(slice: Array<T>, n: number) {
-    return this.from(Array(n).fill(slice).flat());
+  public static repeat<T>(slice: Array<T>, n: number): Vec<T> {
+    return this.from<T>(Array<Array<T>>(n).fill(slice).flat());
   }
 
-  public reverse() {
+  public reverse(): this {
     const slice = this.alloc.slice(0, this.len()).reverse();
 
     this.alloc.splice(0, slice.length, ...slice);
     return this;
   }
 
-  public rotateLeft(mid: number) {
+  public rotateLeft(mid: number): this {
     const slice = this.alloc.slice(0, this.len());
 
     while (mid-- > 0) {
@@ -315,7 +314,7 @@ export class Vec<T> {
     return this;
   }
 
-  public rotateRight(mid: number) {
+  public rotateRight(mid: number): this {
     const slice = this.alloc.slice(0, this.len());
 
     while (mid-- > 0) {
@@ -327,7 +326,7 @@ export class Vec<T> {
     return this;
   }
 
-  public split(p: FnMap<T, boolean>) {
+  public split(p: FnMap<T, boolean>): Vec<unknown> {
     const z = Vec.new();
     const current = Vec.new<T>();
     for (let i = 0; i < this.len(); i++) {
@@ -359,15 +358,15 @@ export class Vec<T> {
     return [l, r];
   }
 
-  public splitFirst() {
+  public splitFirst(): [Vec<T>, Vec<T>] {
     return this.splitAt(1);
   }
 
-  public splitLast() {
+  public splitLast(): [Vec<T>, Vec<T>] {
     return this.splitAt(this.len() - 2);
   }
 
-  public startsWith(...needle: Array<T>) {
+  public startsWith(...needle: Array<T>): boolean {
     if (this.len() === 0 || needle.length === 0) {
       return true;
     }
@@ -382,7 +381,7 @@ export class Vec<T> {
     return true;
   }
 
-  public stripPrefix(...prefix: Array<T>) {
+  public stripPrefix(...prefix: Array<T>): this {
     if (this.startsWith(...prefix)) {
       for (let i = 0; i < prefix.length && this.len(); i++) {
         this.remove(0);
@@ -392,7 +391,7 @@ export class Vec<T> {
     return this;
   }
 
-  public stripSuffix(...prefix: Array<T>) {
+  public stripSuffix(...prefix: Array<T>): this {
     if (this.startsWith(...prefix)) {
       for (let i = 0; i < prefix.length && this.len(); i++) {
         this.pop();
@@ -402,7 +401,7 @@ export class Vec<T> {
     return this;
   }
 
-  public swap(a: number, b: number) {
+  public swap(a: number, b: number): this {
     if (a > this.len()) {
       throw this.oob(a);
     }
@@ -418,22 +417,26 @@ export class Vec<T> {
     return this;
   }
 
-  public chunks(size: number) {
+  public chunks(size: number): Vec<Vec<T>> {
     if (size === 0) {
       throw 'size must be > 0';
     }
 
-    const p = Vec.new();
+    const p = Vec.new<Vec<T>>();
     for (let i = 0; i < this.len(); i += size) {
-      const n = Vec.new();
+      const n = Vec.new<T>();
       for (let j = 0; j < size && j < this.len(); j++) {
-        n.push(this.get(j + i));
+        n.push(this.get(j + i).unwrap());
       }
+
+      p.push(n);
     }
+
+    return p;
   }
 
-  public static from<T>(i: Iterable<T>) {
-    const p = this.new();
+  public static from<T>(i: Iterable<T>): Vec<T> {
+    const p = this.new<T>();
     for (const value of i) {
       p.push(value);
     }
